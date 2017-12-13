95f3393b1797d851bae63d80a776eb8c14ed16a2
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-12-13 09:57:53 +0100

    fix data nack policy

--- app/src/services/data-queue.service.js
+++ app/src/services/data-queue.service.js
@@ -1,68 +1,37 @@
 const logger = require('logger');
-const config = require('config');
-const amqp = require('amqplib');
+const QueueService = require('services/queue.service');
 const writerService = require('services/writer.service');
 const ElasticError = require('errors/elastic.error');
-
-const {
-    DATA_QUEUE
-} = require('app.constants');
+const { DATA_QUEUE } = require('app.constants');
 
 
-class DataQueueService {
+class DataQueueService extends QueueService {
 
     constructor() {
-        logger.info(`Connecting to queue ${DATA_QUEUE}`);
-        try {
-            this.init().then(() => {
-                logger.info('Connected');
-            }, (err) => {
-                logger.error(err);
-                process.exit(1);
-            });
-        } catch (err) {
-            logger.error(err);
-        }
-    }
-
-    async init() {
-        const conn = await amqp.connect(config.get('rabbitmq.url'));
-        this.channel = await conn.createConfirmChannel();
-        const q = DATA_QUEUE;
-        this.channel.assertQueue(q, {
-            durable: true
-        });
-        this.channel.prefetch(1);
-        logger.info(` [*] Waiting for messages in ${q}`);
-        this.channel.consume(q, this.consume.bind(this), {
-            noAck: false,
-
-        });
+        super(DATA_QUEUE, true);
     }
 
     async consume(msg) {
         try {
             logger.debug('Message received');
             const message = JSON.parse(msg.content.toString());
-            
             await writerService.processMessage(message);
             this.channel.ack(msg);
             logger.info('Message processed successfully');
         } catch (err) {
-            logger.error(err);
+            // Accept the message
+            this.channel.ack(msg);
+            // check if ElasticError
+            // in these cases we do not count
             if (err instanceof ElasticError) {
-                logger.error('Error processing message', err);
-                this.channel.nack(msg);
-                return;
-            }
-            const retries = msg.fields.deliveryTag;
-            if (retries < 10) {
-                this.channel.nack(msg);
+                this.returnMsg(msg);
             } else {
-                this.channel.ack(msg);
+                const retries = msg.properties.headers['x-redelivered-count'] || 0;
+                if (retries < 10) {
+                    this.returnMsg(msg);
+                }
             }
         }
-
     }
 
 }
