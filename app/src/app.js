e55caa633ebc4045d63846c75229f98a741ebe5a
Tiago Garcia <tiagojsag@gmail.com>
2019-03-26 12:50:39 +0100

    Basic test setups

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,8 @@
 const logger = require('logger');
 
+// const nock = require('nock');
+// nock.recorder.rec();
+
 logger.debug('Initializing doc-writer');
 require('services/data-queue.service');
 require('services/status-queue.service');
